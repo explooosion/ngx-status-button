499c7ea960456a0299d2ab2ee616183c0cb503e1
Robby <ta7382@gmail.com>
2018-10-29 02:57:48 +0800

    Update ngx service and next animate

--- src/components/ngx-status-button.component.ts
+++ src/components/ngx-status-button.component.ts
@@ -1,60 +1,44 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import { NgxStatusButtonService } from '../services/ngx-status-button.service';
-
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'ngx-status-button',
     templateUrl: './ngx-status-button.component.html',
     styleUrls: ['./ngx-status-button.component.scss']
 })
-export class NgxStatusButtonComponent implements OnInit {
+export class NgxStatusButtonComponent implements OnInit, OnChanges {
 
+    // Status button name
     @Input() public stepNameI: String = 'Pay';
     @Input() public stepNameII: String = 'Processing';
     @Input() public stepNameIII: String = 'Success!';
     @Input() public stepNameIV: String = 'Proceed';
 
-    @Input() public status: number;
+    // Step 1~4 to control animation
+    @Input() public step: number;
 
-    public statusClass: string;
+    public statusClass: object;
 
     constructor(
         private ngxService: NgxStatusButtonService,
     ) { }
 
     ngOnInit(): void {
-        this.status = this.ngxService.status;
-        this.statusClass = `status${this.status}`;
+        this.ngxService.getStep().subscribe(
+            step => { this.step = step; }
+        );
+        this.ngxService.getStatus().subscribe(
+            statusclass => { this.statusClass = statusclass; }
+        );
     }
 
-    /**
-     * Return status class
-     */
-    getStatus() {
-        return {
-            status2: this.statusClass === 'status2',
-            status3: this.statusClass === 'status3',
-            status4: this.statusClass === 'status4',
-        };
-    }
+    ngOnChanges(obj: any) {
+        const { step } = obj;
+        // tslint:disable-next-line:curly
+        if (typeof step === 'undefined') return;
+        // tslint:disable-next-line:curly
+        if (typeof step.previousValue === 'undefined' || step.firstChange) return;
 
-    public onStatusI(): void {
-        this.onStatusII();
-
-        // debug animation
-        setTimeout(() => this.onStatusIII(), 3500);
-        setTimeout(() => this.onStatusIV(), 5000);
-    }
-
-    public onStatusII(): void {
-        this.statusClass = 'status2';
-    }
-
-    public onStatusIII(): void {
-        this.statusClass = 'status3';
-    }
-
-    public onStatusIV(): void {
-        this.statusClass = 'status4';
+        this.ngxService.next();
     }
 }
--- src/services/ngx-status-button.service.ts
+++ src/services/ngx-status-button.service.ts
@@ -1,16 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class NgxStatusButtonService {
 
-    public statusArray: Array<number> = [1, 2, 3, 4];
-    public status = 1;
+    public statusClass: any = {
+        'status2': false,
+        'status3': false,
+        'status4': false,
+    };
 
-    public update(status: number): number {
-        this.status = status;
-        return this.status;
+    public statusStep = 1;
+
+    public getStep(): Observable<number> {
+        return of(this.statusStep);
     }
 
+    public getStatus(): Observable<object> {
+        return of(this.statusClass);
+    }
+
+    public update(status: string) {
+        Object.keys(this.statusClass).forEach((key: string) => {
+            this.statusClass[key] = status === key ? true : false;
+        });
+    }
+
+    public next() {
+        // tslint:disable-next-line:curly
+        if (this.statusStep >= 4) return;
+
+        this.statusStep++;
+        switch (this.statusStep) {
+            case 1:
+                this.update('');
+                break;
+            case 2:
+                this.update('status2');
+                break;
+            case 3:
+                this.update('status3');
+                break;
+            case 4:
+                this.update('status4');
+                break;
+            default:
+                this.update('');
+                break;
+        }
+        // debug animation
+        // setTimeout(() => this.onStatusIII(), 3500);
+        // setTimeout(() => this.onStatusIV(), 5000);
+    }
 }
