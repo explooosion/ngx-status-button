50d3b268e5d492474880ef2d4a3cca34f2c8eff3
Robby <ta7382@gmail.com>
2018-10-29 02:59:23 +0800

    Update example title

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'In-Button-Status';
+  title = 'ngx-status-button';
   step = 1;
 
   onNext() {
